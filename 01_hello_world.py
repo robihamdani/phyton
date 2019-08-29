898971c0194a0ab2079dc95bccb745ebb89ec2e6
robihamdani <robi4us@gmail.com>
2019-08-29 13:35:28 +0700

    Penamabahan Numbers

--- 01_hello_world.py
+++ 01_hello_world.py
@@ -1,2 +1,4 @@
+# sumber belajar dari w3shchools.com
+
 print("Hello World")
 print("I'm Learning Phyton For The Future")
---
+++ 04_numbers.py
@@ -0,0 +1,55 @@
+
+# variable integer
+# x = 1
+# y = 99999999999
+# z = -4432234
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+
+# float integer
+# x = 1.10
+# y = 1.0
+# z = -35.59
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# varibles complex
+# x = 3+5j
+# y = 5j
+# z = -5j
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+
+# Type Convesion
+# x = 1  # int
+# y = 2.8  # float
+# z = 1j  # complex
+
+# convert from int to float:
+# a = float(x)
+
+# convert from float to int:
+# b = int(y)
+
+# convert from int to complex:
+# c = complex(x)
+
+# print(a)
+# print(b)
+# print(c)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+import random
+
+print(random.randrange(1, 10))
