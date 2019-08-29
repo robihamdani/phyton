c138cf216a5c94797a104561a93526f1a175aa73
robihamdani <robi4us@gmail.com>
2019-08-29 13:26:40 +0700

    Upload Variables

---
+++ 03_variables.py
@@ -0,0 +1,44 @@
+# Declare Variable
+# x = 5
+# y = "john"
+# print(x)
+# print(y)
+
+
+# Asign Value Multiple
+# x, y, z = "Orange", "Banana", "Durian"
+# print(x)
+# print(y)
+# print(z)
+
+# x = "Python is "
+# y = "awesome"
+# z = x + y
+# print(z)
+
+# mathematical
+# x = 5
+# y = 5
+# print(x+y)
+
+
+# basic function
+# x = "awesome"
+
+
+# def myfunc():
+#     print("Phyton is" + x)
+
+
+# myfunc()
+
+
+# global variable function
+def myfunc():
+    global x
+    x = "fantastic"
+
+
+myfunc()
+
+print("Python is " + x)
